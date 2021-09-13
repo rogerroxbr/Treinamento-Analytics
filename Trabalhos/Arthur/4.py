573eaf16b5510587e710783a25b4f186d309f80b
Arthur Ogg <arthurdiesel@outlook.com>
2021-09-13 09:38:15 -0300

    ~4.py +5.py +6.py

--- Trabalhos/Arthur/4.py
+++ Trabalhos/Arthur/4.py
@@ -1,23 +1,36 @@
 print("I will now count my chickens:")
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+# 25 mais 30 dividido por 6
+print("Hens", 25.0 + 30.0 / 6.0)
+
+# Resultado de: 100 menos 25 multiplicado por 3 com o resto da divisão por 4
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
 
 print("Now I will count the eggs:")
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# Resultado de: 3 mais 2 mais 1 menos 5 mais 4 o resto da divisão desse resultado por 2 menos 1 dividido por 4 somado com 6
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
 print("Is it true that 3 + 2 < 5 - 7?")
 
-print(3 + 2 < 5 - 7)
+# Verificando se 3 + 2 é menor que 5 - 7
+print(3.0 + 2.0 < 5.0 - 7.0)
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+# Print com o resultado da soma de 3 e 2
+print("What is 3 + 2?", 3.0 + 2.0) 
+
+# Print com o resultado da subtração de 5 e 7
+print("What is 5 - 7?", 5.0 - 7.0)
 
 print("Oh, that's why it's False.")
 
 print("How about some more.")
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+# Verificando se 5 é maior que -2
+print("Is it greater?", 5.0 > -2.0)
+
+# Verificando se 5 é maior ou igual a -2
+print("Is it greater or equal?", 5.0 >= -2.0)
+
+# Verificando se 5 é menor ou igual a -2
+print("Is it less or equal?", 5.0 <= -2.0)
