20788078a3821c8392099b32213dfbdc69dba450
Pedro Pombo <pedro.pombo@brf.com>
2021-09-13 09:33:04 -0300

    Added Ex 4.2

--- Trabalhos/Pedro Pombo/Ex4.py
+++ Trabalhos/Pedro Pombo/Ex4.py
@@ -1,23 +1,34 @@
+#  Printando a primeira linha.
 print('I will now count my chickens')
 
+# Contando as galinhas
 print('Hens', 25+30/6)
-print('Roosters', 100-25*3%4)
+# Contando os galos
+print('Roosters', 100 - 25 * 3 % 4)
 
+# Falando que vai contar os ovos
 print("Now i will count my eggs:")
 
-print((3 + 2 + 1 - 5 +4 % 2 - 1 / 4 + 6))
+# Somando os ovos
+print((3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
 
+# Printando uma inequação para ver se é verdade.
 print("Is it true that 3 + 2 < 5-7")
 
-print(3 + 2 < 5 - 7)
+# Checando a inequação
+print(3.0 + 2.0 < 5.0 - 7.0)
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7", 5 - 7)
+# Fazendo as contas
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7", 5.0 - 7.0)
 
+# Falando que é falso
 print("Oh, that's why it's False.")
 
+# Falando sobre mais contas
 print("How about some more.")
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+# Checando os números
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
