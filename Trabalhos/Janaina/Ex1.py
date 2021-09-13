d6800c850cab9f8c4a81477029b3a56321e567ef
Janaina_Cunico <janaina.cunico@brf.com>
2021-09-13 12:41:35 -0300

    Salvando exercicios

---
+++ Trabalhos/Janaina/Ex1.py
@@ -0,0 +1,8 @@
+# A coment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print ("I could have code like this.") # and the coment is ignored
+
+# you can also use a comment to "disable" or comment out code:
+
+print("This will run")
---
+++ Trabalhos/Janaina/Ex_Desafio.py
@@ -0,0 +1,14 @@
+nome = "Janaina"
+idade = 38
+ano_atual = 2021
+peso = 63
+altura = 1,60
+
+ano_nasc = ano_atual - idade
+print('O ano de nascimento é:' ,ano_nasc)
+
+peso = 63
+altura = 1,60
+
+IMC = (altura*altura)/peso
+print(IMC)
