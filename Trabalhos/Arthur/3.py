1c34f8c899364e926aa67ada1ffad9c00c888ce0
Arthur Ogg <arthurdiesel@outlook.com>
2021-09-13 09:17:20 -0300

    +3.py +4.py ~1.py (PEP8) ~2.py (PEP8)

---
+++ Trabalhos/Arthur/3.py
@@ -0,0 +1,12 @@
+"""
+Tipos de dados
+str - string - textos 'Assim' "Assim"
+int - inteiro - 123456 0 -10 -20 -50 -60 -15000 1500
+float - real/ponto flutuante - 10.50 1.5 -10.10 -50.93 0.0
+bool - booleano/lógico - True/False 10 == 10
+"""
+
+print('Luiz', type('Luiz'))
+print(10 , type(10))
+print(25.23, type(25.23))
+print('l' == 'L', type('l' == 'L'))
