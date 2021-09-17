fdb35af41011759795536caa198099f767d374f5
Mists <lmists13@gmail.com>
2021-09-17 00:02:57 -0300

    Exercises of days 3 and 4

--- Trabalhos/Millena/day1/Ex8.py
+++ Trabalhos/Millena/day1/Ex8.py
@@ -4,24 +4,25 @@ current_year = 2021
 height = float(input("What's your height? "))
 weight = float(input("What's your weight? "))
 yearOfBirth = current_year - age
-IMC = weight / (height ** 2)
+imc = weight / (height ** 2)
 
 print(f'The year of birth of: {name} is {yearOfBirth}, based on Current year {current_year} and their age {age}.'
-      f' Your IMC is {IMC:.2f}. Based on the values {height} and {weight}.')
+      f' Their IMC is {imc:.2f}. Based on the values {height} and {weight}.')
+
+message = ''
+
+if imc < 16.9:
+    message = "Muito abaixo do peso"
+elif imc < 24.9:
+    message = "Peso normal"
+elif imc < 29.9:
+    message =  "Acima do peso"
+elif imc < 34.9:
+    message =  "Obesidade Grau I"
+elif imc < 40:
+    message =  "Obesidade Grau II"
+else:
+    message = "Obesidade Grau III"
 
 
-def weight_range(imc):
-    if imc < 16.9:
-        return "Muito abaixo do peso"
-    if imc < 24.9:
-        return "Peso normal"
-    if imc < 29.9:
-        return "Acima do peso"
-    if imc < 34.9:
-        return "Obesidade Grau I"
-    if imc < 40:
-        return "Obesidade Grau II"
-    return "Obesidade Grau III"
-
-
-print(f'Result: your IMC result is {weight_range(IMC)}. ')
+print(f'Result: your IMC result is {message}. ')
