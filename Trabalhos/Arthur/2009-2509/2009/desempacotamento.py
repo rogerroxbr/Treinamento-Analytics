b90730cf94c6fae763cb62e96df3ead8d74cd399
Arthur Ogg <arthurdiesel@outlook.com>
2021-09-20 10:57:10 -0300

    2009

---
+++ Trabalhos/Arthur/2009-2509/2009/desempacotamento.py
@@ -0,0 +1,11 @@
+lista = ['Luiz', 'João', 'Maria', 1,2,3,4,5,6,7,9,100]
+n1, n2, n3, *outra_lista = lista
+print(n1,n2,n3, outra_lista) 
+
+"""
+n1 = Luiz
+n2 = João
+n3 = Maria
+
+outra_lista = [1,2,3,4,5,6,7,9,100]
+"""
